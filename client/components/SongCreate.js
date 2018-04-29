f2a1d999255ce40ebc95296b76ca4d41050932e5
Kelvan Ince <incekelvan@gmail.com>
2018-04-28 19:23:28 -0500

    Use the refetchQueries property on the mutate object to send the query along with the submission so that the data is returned to the user instantaneously

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -17,13 +20,17 @@ class SongCreate extends Component {
     this.props.mutate({
       variables: {
         title: this.state.title
-      }
-    });
+      },
+      refetchQueries: [{ query: query }]
+    }).then(() => hashHistory.push('/'))
   };
 
   render() {
     return (
       <div>
+        <Link to="/">
+          Back
+        </Link>
         <h3>Create a new song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
